f225ef9eccd710cdf14256fe68b2afd17b81183f
ghadeer-078 <90647463+ghadeer-078@users.noreply.github.com>
2021-12-21 14:53:58 +0300

    Flickr Browser App... Create a Flickr application that does the following: - Makes us of Flickr API to fetch photos based on a search - Makes use of Glide to display photos - Displays photo thumbnails in a Recycler View along with their title - Allows users to view large versions of the thumbnails by tapping on them
    You will need to do your own research regarding the Flickr API. You will have to sign up to get your API Key, read the API docs to learn how to use the API, learn how to pass in the correct parameters, convert the output to JSON, and more.
    You can find the API here: https://www.flickr.com/services/developer/api/

---
+++ app/src/main/java/com/example/flickr_browser_app/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.flickr_browser_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.flickr_browser_app.API.APIClient
+import com.example.flickr_browser_app.API.APIInterface
+import com.example.flickr_browser_app.Model.PhotoCollection
+import com.example.flickr_browser_app.Resource.Adapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var photosList: PhotoCollection
+    lateinit var photosRecyclerView: RecyclerView
+    lateinit var searchEditText: EditText
+    lateinit var searchButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loadRV()
+        connectView()
+
+        searchButton.setOnClickListener {
+            if (searchEditText.text.toString().isNotEmpty()) {
+                getPhotos(searchEditText.text.toString())
+                searchEditText.setText("")
+            } else
+                Toast.makeText(this, "Please enter a search keyword", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun getPhotos(searchKey: String) {
+
+        val apiInterface = APIClient().getClient()?.create(APIInterface::class.java)
+        val call: Call<PhotoCollection?>? = apiInterface!!.getPhotos(searchKey)
+        call?.enqueue(object : Callback<PhotoCollection?> {
+            override fun onResponse(
+                call: Call<PhotoCollection?>,
+                response: Response<PhotoCollection?>
+            ) {
+                photosList = response.body()!!
+                photosRecyclerView.adapter = Adapter(this@MainActivity, photosList)
+            }
+
+            override fun onFailure(call: Call<PhotoCollection?>, t: Throwable) {
+                Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
+                Log.d("The error message", "${t.message}")
+                call.cancel()
+            }
+        })
+
+    }
+
+
+    private fun loadRV() {
+        photosRecyclerView = findViewById(R.id.photos_rv)
+        photosRecyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun connectView() {
+        searchEditText = findViewById(R.id.search_et)
+        searchButton = findViewById(R.id.search_btn)
+    }
+}
---
+++ app/src/main/java/com/example/flickr_browser_app/Model/PhotoCollection.kt
@@ -0,0 +1,36 @@
+package com.example.flickr_browser_app.Model
+
+import com.google.gson.annotations.SerializedName
+
+data class PhotoCollection(
+    @SerializedName("photos")
+    val photos: Photos,
+
+    @SerializedName("stat")
+    val stat: String
+)
+
+data class Photos(
+    @SerializedName("photo")
+    val photo: List<Photo>,
+
+    @SerializedName("total")
+    val total: Int
+)
+
+data class Photo(
+    @SerializedName("id")
+    val id: String,
+
+    @SerializedName("owner")
+    val owner: String,
+
+    @SerializedName("secret")
+    val secret: String,
+
+    @SerializedName("server")
+    val server: String,
+
+    @SerializedName("title")
+    val title: String
+)
